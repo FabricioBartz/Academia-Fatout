66ff00284003af5554e3ac1ba22a832e1df7c661
Fabricio Fiss Bartz <fabriciofissbartz@gmail.com>
2026-01-07 01:30:02 -0300

    novo banco de dados

--- bancodedados.sql
+++ bancodedados.sql
@@ -27,6 +27,7 @@ DROP TABLE IF EXISTS `aluno`;
 CREATE TABLE `aluno` (
   `cpf` char(11) NOT NULL,
   `objetivo` varchar(255) DEFAULT NULL,
+  `foto_perfil` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`cpf`),
   CONSTRAINT `FK_aluno_pessoa` FOREIGN KEY (`cpf`) REFERENCES `pessoa` (`cpf`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -38,7 +39,7 @@ CREATE TABLE `aluno` (
 
 LOCK TABLES `aluno` WRITE;
 /*!40000 ALTER TABLE `aluno` DISABLE KEYS */;
-INSERT INTO `aluno` VALUES ('02673548900','Perder peso'),('02784570000','ganhar massa'),('08897510064','ficar definido'),('12345678901','ganhar massa'),('55555555555','Condicionamento Físico'),('98765432100','Emagrecimento');
+INSERT INTO `aluno` VALUES ('02673548900','Perder peso',NULL),('02784570000','ganhar massa',NULL),('08897510064','ficar definido',NULL),('12345678901','ganhar massa','12345678901.png'),('55555555555','Condicionamento Físico',NULL),('98765432100','Emagrecimento',NULL);
 /*!40000 ALTER TABLE `aluno` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -66,7 +67,7 @@ CREATE TABLE `aluno_turma` (
 
 LOCK TABLES `aluno_turma` WRITE;
 /*!40000 ALTER TABLE `aluno_turma` DISABLE KEYS */;
-INSERT INTO `aluno_turma` VALUES ('12345678901',1),('12345678901',2),('02673548900',7);
+INSERT INTO `aluno_turma` VALUES ('12345678901',1),('02784570000',2),('12345678901',2),('12345678901',5),('02673548900',7),('02784570000',7),('12345678901',7),('12345678901',8),('12345678901',9);
 /*!40000 ALTER TABLE `aluno_turma` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -100,7 +101,7 @@ CREATE TABLE `avaliacao_fisica` (
   KEY `IDX_avaliacao_instrutor` (`cpf_instrutor`),
   CONSTRAINT `FK_avaliacao_aluno` FOREIGN KEY (`cpf_aluno`) REFERENCES `aluno` (`cpf`) ON DELETE SET NULL,
   CONSTRAINT `FK_avaliacao_instrutor` FOREIGN KEY (`cpf_instrutor`) REFERENCES `instrutor` (`cpf`) ON DELETE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -109,7 +110,7 @@ CREATE TABLE `avaliacao_fisica` (
 
 LOCK TABLES `avaliacao_fisica` WRITE;
 /*!40000 ALTER TABLE `avaliacao_fisica` DISABLE KEYS */;
-INSERT INTO `avaliacao_fisica` VALUES (1,'12345678901','11111111111','2025-12-14',88,1.77,28.088991030674453,120,85,77,88,77,65,54,87,57,''),(2,'02673548900','11111111111','2025-12-16',75.5,1.75,24.653061224489797,100,85,95,36,35,58,57,40,39,'Texto de observação'),(4,'12345678901','11111111111','2025-12-17',95,1.75,31.020408163265305,100,85,95,40,40,60,60,40,40,''),(5,'55555555555','22222222222','2025-12-17',80,1.9,22.1606648199446,100,90,95,40,40,60,60,45,45,''),(6,'98765432100','11111111111','2025-12-25',60,1.6,23.437499999999996,90,65,92,25,25,50,50,32,32,'');
+INSERT INTO `avaliacao_fisica` VALUES (1,'12345678901','11111111111','2025-12-14',88,1.77,28.088991030674453,120,85,77,88,77,65,54,87,57,'testando o campo de observações'),(2,'02673548900','11111111111','2025-12-16',75.5,1.75,24.653061224489797,100,85,95,36,35,58,57,40,39,'Texto de observação'),(4,'12345678901','11111111111','2025-12-17',95,1.74,31.37798916633637,100,85,95,40,40,60,60,40,40,''),(5,'55555555555','22222222222','2025-12-17',80,1.9,22.1606648199446,100,90,95,40,40,60,60,45,45,''),(6,'98765432100','11111111111','2025-12-25',60,1.6,23.437499999999996,90,65,92,25,25,50,50,32,32,''),(7,'02784570000','11111111111','2025-12-28',58,1.65,21.30394857667585,80,70,80,32,32,50,50,38,38,'avaliação de teste');
 /*!40000 ALTER TABLE `avaliacao_fisica` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -128,7 +129,7 @@ CREATE TABLE `exercicio` (
   `instrucoes` text,
   `descricao` text,
   PRIMARY KEY (`id_exercicio`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -137,7 +138,7 @@ CREATE TABLE `exercicio` (
 
 LOCK TABLES `exercicio` WRITE;
 /*!40000 ALTER TABLE `exercicio` DISABLE KEYS */;
-INSERT INTO `exercicio` VALUES (1,'Supino Reto','Barra e Banco','Peito','Deite-se no banco, segure a barra com as mãos afastadas na largura dos ombros. Desça a barra até o peito e empurre para cima.','Exercício básico para desenvolvimento do peitoral.\r\n'),(2,'Agachamento Livre','Barra','Pernas','Posicione a barra nos ombros, desça flexionando os joelhos até formar 90 graus e retorne à posição inicial.','Exercício fundamental para desenvolvimento das pernas'),(3,'Agachamento Livre com Barra','Rack de Agachamento e Barra','Pernas (Quadríceps e Glúteos) ','Em pé, segure a barra na altura da parte superior do peito com as mãos um pouco além da largura dos ombros. Empurre a barra para cima da cabeça até os braços esticarem totalmente, mantendo o abdômen e glúteos bem contraídos para não curvar as costas.','O clássico teste de força de empurrar para a parte superior do corpo. Diferente do supino, este exige muito mais estabilização de todo o tronco por ser feito em pé.');
+INSERT INTO `exercicio` VALUES (1,'Supino Reto','Barra e Banco.','Peito','Deite-se no banco, segure a barra com as mãos afastadas na largura dos ombros. Desça a barra até o peito e empurre para cima.','Exercício básico para desenvolvimento do peitoral.\r\n'),(2,'Agachamento Livre','Barra','Pernas','Posicione a barra nos ombros, desça flexionando os joelhos até formar 90 graus e retorne à posição inicial.','Exercício fundamental para desenvolvimento das pernas'),(3,'Agachamento Livre com Barra','Rack de Agachamento e Barra','Pernas (Quadríceps e Glúteos) ','Em pé, segure a barra na altura da parte superior do peito com as mãos um pouco além da largura dos ombros. Empurre a barra para cima da cabeça até os braços esticarem totalmente, mantendo o abdômen e glúteos bem contraídos para não curvar as costas.','O clássico teste de força de empurrar para a parte superior do corpo. Diferente do supino, este exige muito mais estabilização de todo o tronco por ser feito em pé.'),(4,'Remada Curvada','Barra','Dorsais (costas), romboides e bíceps.','Incline o tronco à frente, mantenha as costas retas e puxe a barra em direção ao abdômen, retraindo as escápulas.','Excelente para construir densidade nas costas e melhorar a postura.');
 /*!40000 ALTER TABLE `exercicio` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -162,7 +163,7 @@ CREATE TABLE `exercicio_plano` (
   KEY `IDX_exercicio_plano_plano` (`id_plano`),
   CONSTRAINT `FK_exercicio_plano_exercicio` FOREIGN KEY (`id_exercicio`) REFERENCES `exercicio` (`id_exercicio`) ON DELETE CASCADE,
   CONSTRAINT `FK_exercicio_plano_plano` FOREIGN KEY (`id_plano`) REFERENCES `plano_de_treino` (`id_plano`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=105 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -171,7 +172,7 @@ CREATE TABLE `exercicio_plano` (
 
 LOCK TABLES `exercicio_plano` WRITE;
 /*!40000 ALTER TABLE `exercicio_plano` DISABLE KEYS */;
-INSERT INTO `exercicio_plano` VALUES (85,2,4,5,12,NULL,NULL,NULL),(86,2,4,2,12,NULL,NULL,NULL),(87,2,7,5,12,NULL,NULL,NULL),(88,2,7,2,12,NULL,NULL,NULL),(92,2,6,5,12,NULL,NULL,NULL),(93,1,6,1,10,NULL,NULL,NULL);
+INSERT INTO `exercicio_plano` VALUES (92,2,6,5,12,NULL,NULL,NULL),(93,1,6,1,10,NULL,NULL,NULL),(96,4,8,5,12,NULL,NULL,NULL),(101,2,7,5,12,NULL,NULL,NULL),(102,2,7,2,12,NULL,NULL,NULL),(103,2,4,5,12,NULL,NULL,NULL),(104,2,4,2,12,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `exercicio_plano` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -186,6 +187,7 @@ CREATE TABLE `instrutor` (
   `cpf` char(11) NOT NULL,
   `dia_que_comecou_trabalhar` date DEFAULT NULL,
   `is_admin` tinyint(1) NOT NULL DEFAULT '0',
+  `foto_perfil` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`cpf`),
   CONSTRAINT `FK_instrutor_pessoa` FOREIGN KEY (`cpf`) REFERENCES `pessoa` (`cpf`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -197,7 +199,7 @@ CREATE TABLE `instrutor` (
 
 LOCK TABLES `instrutor` WRITE;
 /*!40000 ALTER TABLE `instrutor` DISABLE KEYS */;
-INSERT INTO `instrutor` VALUES ('11111111111','2020-03-15',1),('22222222222','2024-10-12',0),('33333333333','2024-12-27',1);
+INSERT INTO `instrutor` VALUES ('11111111111',NULL,1,'11111111111.png'),('22222222222','2024-10-12',0,'22222222222.jpg'),('33333333333','2024-12-27',1,NULL);
 /*!40000 ALTER TABLE `instrutor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -230,7 +232,7 @@ CREATE TABLE `pessoa` (
 
 LOCK TABLES `pessoa` WRITE;
 /*!40000 ALTER TABLE `pessoa` DISABLE KEYS */;
-INSERT INTO `pessoa` VALUES ('02673548900','Aluno Teste 05','alunoteste05@gmail.com','51998758230','B00,.','2000-04-16',NULL,NULL,'2025-12-16'),('02784570000','Fabricio Bartz','fabricio@aluno.com','51996568623','B123,.','1995-07-16',NULL,NULL,'2024-12-27'),('08897510064','Arthur Alves','arthur@aluno.com','53994755625','123','2001-09-22',NULL,NULL,'2025-12-27'),('11111111111','Carlos Silva','carlos@academia.com','11999999999','123','1980-01-01',NULL,NULL,NULL),('12345678901','João Silva','joao@email.com','11987654321','123','1995-03-14',NULL,NULL,'2025-12-14'),('22222222222','Ana Costa','ana@academia.com','11988888882','123','1985-05-20',NULL,NULL,'2024-10-12'),('33333333333','Instrutor teste','instrutorteste@academia.com','53998169734','B123,.','1995-02-23',NULL,NULL,'2024-12-27'),('55555555555','Pedro Oliveira','pedro@email.com','11955555555','123','1990-11-30',NULL,NULL,'2025-12-17'),('98765432100','Maria Santos','maria@email.com','11976543210','123','1992-07-22',NULL,NULL,'2025-12-25');
+INSERT INTO `pessoa` VALUES ('02673548900','Aluno Teste 05','alunoteste05@gmail.com','51998758232','B00,.','2000-04-16',NULL,NULL,'2025-11-16'),('02784570000','Fabricio Bartz','fabricio@aluno.com','51996568623','B123,.','1995-07-16',NULL,NULL,'2024-12-27'),('08897510064','Arthur Alves','arthur@aluno.com','53994755625','123','2001-09-22',NULL,NULL,'2025-12-27'),('11111111111','Carlos Silva','carlos@academia.com','11999999994','123','1990-02-23',NULL,NULL,'2020-09-15'),('12345678901','João Silva','joao@email.com','11987654321','123','1995-03-14',NULL,NULL,'2025-12-14'),('22222222222','Ana Costa','ana@academia.com','11988888883','123','1985-05-20',NULL,NULL,'2024-10-12'),('33333333333','Instrutor teste','instrutorteste@academia.com','53998169734','B123,.','1995-02-23',NULL,NULL,'2024-12-27'),('55555555555','Pedro Oliveira','pedro@email.com','11955555555','123','1990-11-30',NULL,NULL,'2025-12-17'),('98765432100','Maria Santos','maria@email.com','11976543210','123','1992-07-22',NULL,NULL,'2025-12-25');
 /*!40000 ALTER TABLE `pessoa` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -250,12 +252,13 @@ CREATE TABLE `plano_de_treino` (
   `nome` varchar(255) DEFAULT NULL,
   `observacoes` varchar(500) DEFAULT NULL,
   `dias_semana` varchar(50) DEFAULT NULL,
+  `data_termino` date DEFAULT NULL,
   PRIMARY KEY (`id_plano`),
   KEY `IDX_plano_aluno` (`cpf_aluno`),
   KEY `IDX_plano_instrutor` (`cpf_instrutor`),
   CONSTRAINT `FK_plano_aluno` FOREIGN KEY (`cpf_aluno`) REFERENCES `aluno` (`cpf`) ON DELETE SET NULL,
   CONSTRAINT `FK_plano_instrutor` FOREIGN KEY (`cpf_instrutor`) REFERENCES `instrutor` (`cpf`) ON DELETE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -264,7 +267,7 @@ CREATE TABLE `plano_de_treino` (
 
 LOCK TABLES `plano_de_treino` WRITE;
 /*!40000 ALTER TABLE `plano_de_treino` DISABLE KEYS */;
-INSERT INTO `plano_de_treino` VALUES (4,'12345678901','11111111111','2025-12-16',NULL,'treino do joao','teste para ver se esta funcionando o campo observação','QUA,SEX'),(6,'02673548900','11111111111','2025-12-16',NULL,'plano de treino teste','teste para ver se esta funcionando o campo observação','TER,QUA,SAB,DOM'),(7,'12345678901','11111111111','2025-12-17',NULL,'treino de teste','teste para ver se esta funcionando o campo observação','QUA,QUI,SEX');
+INSERT INTO `plano_de_treino` VALUES (4,'12345678901','11111111111','2025-12-16',NULL,'treino do joao','teste para ver se esta funcionando o campo observação','QUA,SEX','2026-01-06'),(6,'02673548900','11111111111','2025-12-16',NULL,'plano de treino teste','teste para ver se esta funcionando o campo observação','TER,QUA,SAB,DOM',NULL),(7,'12345678901','11111111111','2025-12-17',NULL,'treino de teste','teste para ver se esta funcionando o campo observação','SEG,TER,QUA,QUI,SEX,SAB,DOM',NULL),(8,'02784570000','11111111111','2025-12-28',NULL,'treino do Fabricio','teste para ver se esta funcionando o campo observação','SEG,TER,QUA,QUI,SEX,SAB,DOM',NULL);
 /*!40000 ALTER TABLE `plano_de_treino` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -286,7 +289,7 @@ CREATE TABLE `turma` (
   PRIMARY KEY (`id_turma`),
   KEY `IDX_turma_instrutor` (`cpf_instrutor`),
   CONSTRAINT `FK_turma_instrutor` FOREIGN KEY (`cpf_instrutor`) REFERENCES `instrutor` (`cpf`) ON DELETE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -295,7 +298,7 @@ CREATE TABLE `turma` (
 
 LOCK TABLES `turma` WRITE;
 /*!40000 ALTER TABLE `turma` DISABLE KEYS */;
-INSERT INTO `turma` VALUES (1,'11111111111','Funcional Matinal','Treino funcional focado em condicionamento físico e mobilidade.','2025-12-20','07:00:00',18),(2,'22222222222','Spinning','Aula de ciclismo indoor com música animada','2025-12-21','18:30:00',20),(5,'11111111111','turma do Carlos','abc','2025-12-17','10:00:00',19),(7,'22222222222','Natação para iniciantes','natação','2026-01-17','10:00:00',5);
+INSERT INTO `turma` VALUES (1,'11111111111','Funcional Matinal','Treino funcional focado em condicionamento físico e mobilidade.','2025-12-20','07:00:00',3),(2,'22222222222','Spinning','Aula de ciclismo indoor com música animada','2025-12-21','18:30:00',20),(5,'11111111111','turma do Carlos','abc','2025-12-17','10:00:00',19),(7,'22222222222','Natação para iniciantes','natação','2026-01-17','10:00:00',5),(8,'11111111111','Yoga','Fortaleça o corpo e acalme a mente. Nesta turma, exploramos o Yoga de forma dinâmica, focando no alinhamento corporal, ganho de mobilidade e fortalecimento muscular consciente.','2026-01-10','10:00:00',10),(9,'11111111111','teste 2','turma teste','2026-03-12','15:00:00',5);
 /*!40000 ALTER TABLE `turma` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -308,4 +311,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-12-27  5:05:11
+-- Dump completed on 2026-01-07  1:29:25
